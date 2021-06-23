fix issue with gradient descent

helpers.py
@@ -1,22 +1,28 @@
 import numpy as np
-
-
+import matplotlib.pyplot as plt
 
 # Compute cost for linear regression
-def compute_cost(X, Y, theta=np.array([[0],[0]])):
-    m = len(Y)
-    k = 1/(2*m)
-    h = np.dot(X, theta) # mx2 . 2x1  = mx1
-    J = k * np.sum((h - Y) ** 2) # mx1 - mx1 = mx1
-    return J
+def compute_cost(X, y, theta=np.array([[0],[0]])):
+    m = len(y)
+    h = np.dot(X,theta)
+    cost = (1.0/(2*m)) * np.dot((h-y).T,(h-y))
+    return float(cost)
 
 # Compute gradient descent to fit theta params for linear regression
-def gradient_descent(X, Y, theta, alpha, iterations):
-    m = len(Y)
-    J_history = []
+def gradient_descent(X, y, theta, alpha, iterations):
+    m = len(y)
+    cost_history = []
     for i in range(iterations):
-        h = np.matmul(X, theta)
-        theta = theta - alpha * (1 / m) * np.transpose(np.matmul(np.transpose(h - Y), X))
-        J_history.append(compute_cost(X, Y, theta))
+        theta_tmp = theta
+        cost_history.append(compute_cost(X, y, theta))
+        for j in range(len(theta_tmp)):
+            h = np.dot(X, theta_tmp)
+            x_j = X[:,j].reshape(m,1)
+            theta_tmp[j] = theta_tmp[j] - (alpha/m)*np.sum((h - y)*x_j)
+        theta = theta_tmp
+    return theta, cost_history
 
-    return theta, J_history
+def show_data(x, y, x_label='x', y_label='y'):
+    plt.scatter(x, y)
+    plt.xlabel(x_label); plt.ylabel(y_label)
+    plt.show()

main.py
@@ -1,32 +1,37 @@
 # % PREDICTION OF PROFITS WITH LINEAR REGRESSION %
 import numpy as np
 import matplotlib.pyplot as plt  # To visualize
-import pandas as pd  # To read data
 
 import helpers
 
 # 1// Init data
 data = np.loadtxt('data/ex1data1.txt', delimiter=',')
+
 m = data.shape[0]
 ones = np.ones((m,1))
-X = data[:,0].reshape(m,1)
-X = np.concatenate((ones, X), axis=1)
+X_tmp = data[:,0]
+X = np.c_[ones, X_tmp]
 y = data[:,1].reshape(m,1)
 
-# 2// Plot raw data
-plt.scatter(data[:,0], data[:,1])
-plt.xlabel('X'); plt.ylabel('y')
-plt.title('Input dataset')
+# helpers.show_data(data[:,0], data[:,1], 'Population of City in 10,000s', 'Profit in $10,000s')
 
 # 3// Fit theta with gradient descent
-iterations = 1500
-alpha = 0.01
 theta_init = np.zeros((2, 1))
-theta, history = helpers.gradient_descent(X, y, theta_init, alpha, iterations)
+
+theta, history = helpers.gradient_descent(X, y, theta_init, 0.1, 45)
+
+print(history)
+
+# helpers.show_data(list(range(0, 1500)), history, 'Iterations', 'Cost')
 
 # 4// Plot a line from slope and intercept
 axes = plt.gca()
-x_vals = np.array(axes.get_xlim())
-y_vals = theta[0] + theta[1] * x_vals
-plt.plot(x_vals, y_vals, '--', color='tab:red')
+x_vals = list(range(0, 45))
+y_vals = history
+plt.plot(x_vals, y_vals, "--")
 plt.show()
+# axes = plt.gca()
+# x_vals = np.array(axes.get_xlim())
+# y_vals = theta[0] + theta[1] * x_vals
+# plt.plot(x_vals, y_vals, "--")
+# plt.show()